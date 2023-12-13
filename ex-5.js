d2899e1edd2cbe5853feef366e3b0fbe3407a88f
Panupong <Pai28212m@gmail.com>
2023-12-13 14:53:56 +0700

    HH:Pai

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,13 @@
-function sum() {
+function sum(...rest) {
   // Start coding here !
+  let total = 0;
+
+  for (let i of rest) {
+    console.log(i);
+    total += i;
+  }
+
+  return total;
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,10 @@
+function checkHeight(height) {
+ 
+  return height ?? "Height is not defined";
+}
+
 let height = undefined;
-let result;
+
+const result = checkHeight(height);
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
